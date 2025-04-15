15f745551b613f24e5250a5f99e383ec822b43cd
moskalevsa <moskalevsa@yandex.ru>
2025-04-15 13:43:16 +0300

    Код функции записи в журнал, поправленный по материалам тестирования н 15.04.2025

--- dzlog.py
+++ dzlog.py
@@ -6,6 +6,9 @@ from idlelib.iomenu import encoding
 import datetime
 
 def write_log(logfile, message : str):
+    """
+    запись сообщения в журнал с указанием времени
+    """
     with open(logfile, 'a', encoding ='utf-8') as file:
         logmessage = f'{datetime.datetime.now().strftime("%d.%m.%Y, %H:%M")} INFO: {message}'
         file.write(logmessage)
