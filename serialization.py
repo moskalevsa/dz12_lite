853eabaf90e1122364b35bccb9fc542dc5d0c78b
moskalevsa <moskalevsa@yandex.ru>
2025-04-19 19:53:27 +0300

    добавление журналирования

--- serialization.py
+++ serialization.py
@@ -15,7 +15,6 @@ from fileinput import filename
 from idlelib.iomenu import encoding
 import time
 from pprint import pprint
-
 import ipykernel
 from xml.etree.ElementTree import indent
 from dzlog import  write_log
@@ -41,7 +40,7 @@ class File_data:
 
     def get_print_str(self):
             """
-            получить f строку для печати    :return:
+            получить f строку для печати
             """
             output1 = f'file name : {self.filename}\n original text : {self.text_isx} \n text : {self.text_proc}\n '
             output2 =f'byte size : {self.filesize}\n last change : {self.lastmod}\n'
@@ -53,7 +52,7 @@ class File_data:
 
 class File_data_list:
     """
-     Класс содержащий список данных классов
+     Класс содержащий список данных файлов
     """
     def __init__(self, f_data_list : list[:File_data]):
         self.f_data_list = f_data_list
@@ -92,80 +91,89 @@ def filedatalist_to_dict (filedata_list :File_data_list):
     return    result_dict
 
 # Определение текущей директории
-print (f'текущая директория: {os.getcwd()}')
+maindir = os.getcwd()
+print (f'текущая директория: {maindir}')
 
 #Смена текущей директории
 os.chdir('poject_root')
-print (f'смена текущей директории на: {os.getcwd()}')
+print (f'смена текущей директории на: {os.getcwd()} ')
 
 # Получение списка исъодных текстов
 #Смена текущей директории
 os.chdir('data/raw')
+print (f'смена текущей директории на: {os.getcwd()} для чтения исходных файлв')
+
+#Смена текущей директории
+os.chdir( maindir)
+os.chdir('poject_root')
+filelog = 'logs/dz_log.log'
+print (f'смена текущей директории на: {os.getcwd()}\n')
+
+#чтение файлов в директорию raw
+#Смена текущей директории
+os.chdir('data/raw')
 print (f'смена текущей директории на: {os.getcwd()}')
 
-file_read_dir = os.getcwd()
-file_list_isx = []
-for fl in os.listdir(file_read_dir):
-    file_list_isx.append(fl)
-print(f'Список исходных файлов заполнен:\n {file_list_isx}')
+file_list_r = os.listdir(os.getcwd())
+print(f'Список файлов заполнен:\n {file_list_r}')
 
-#Получение списка исходных текстов
-text_list_read_isx =[] #Список прочитанных исходных текстов
-for i in range(len(file_list_isx)):
-    flr  = file_list_isx[i]
+#Чтение исходных файлов
+text_read =[] #Список прочитанных текстов
+message_log1 = [] #Список сообщений о прочтнении файла для журналирования
+for i in range(len(file_list_r)):
+    flr  = file_list_r[i]
     encflr = determ_encoding(flr)  # Определение кодировки
     textf = read_file(flr, encr = encflr)
-    text_list_read_isx.append(textf)
-
-print (text_list_read_isx[0])
+    text_read.append(textf)
+    messagef = f'INFO: файл {flr} прочитан\n'
+    message_log1.append(messagef)
 
 # Смена текщей дрректории . на data/processed
-os.chdir('..')
-os.chdir('..')
+os.chdir( maindir)
+os.chdir('poject_root')
+print(os.getcwd())
 os.chdir('data/processed')
 print (f'смена текущей директории на: {os.getcwd()}')
 
-#Получение списка имен файлов
-file_read_dir = os.getcwd()
-file_list_proc = []
-for fl in os.listdir(file_read_dir):
-    file_list_proc.append(fl)
-print(f'Список обработанных файлов заполнен:\n {file_list_proc}')
+#Получение списка имен oбработанных файлов
+file_read_proc = os.listdir(os.getcwd())
+print(f'Список обработанных файлов заполнен:\n {file_read_proc} для чтения преобразованных файлов')
 
 #Получение списка обработанных текстов
-text_list_read_proc =[] #Список прочитанных исходных текстов
-for i in range(len(file_list_proc)):
-    flr  = file_list_proc[i]
+text_read_proc =[] #Список прочитанных исходных текстов
+message_log2 = [] #Список сообщений о прочтнении файла для журналирования
+for i in range(len(file_read_proc)):
+    flr  = file_read_proc[i]
     encflr = determ_encoding(flr)  # Определение кодировки
     textf = read_file(flr,  encflr)
-    text_list_read_proc.append(textf)
+    text_read_proc.append(textf)
+    messagef = f'INFO: файл {flr} прочитан\n'
+    message_log2.append(messagef)
 
 #Получение списка размеров файлов в байтах
 file_list_size = []   #Список размеров файлов в байтах
-for i in range(len(file_list_proc)):
-    flr  = file_list_proc[i]
+for i in range(len(file_read_proc)):
+    flr  = file_read_proc[i]
     fsize = os.path.getsize(flr)
     file_list_size.append(fsize)
-print(f'Список размеров файлов заполнен:\n {file_list_size}')
 
 #Получение даты последнего изменеия файла
 file_list_last = []   #Список размеров файлов в байтах
-for i in range(len(file_list_proc)):
-    flr  = file_list_proc[i]
+for i in range(len(file_read_proc)):
+    flr  = file_read_proc[i]
     md_time = os.path.getmtime(flr)
     rd_time = time.ctime(md_time)
     print(f"Last modified: {rd_time}")
     file_list_last.append(rd_time)
-print(f'Список дат последнего изменеия файлов заполнен:\n {file_list_last}')
-os.chdir('..')
-# Получить список файлоа
+
+# Получить объект список файлоа
 list_faildata = []
-for i in range(len(file_list_proc)):
-    filedata = File_data ( file_list_proc[i]
-                       ,text_list_read_isx[i]
-                       ,text_list_read_proc[i]
-                        ,file_list_size[i]
-                        ,file_list_last[i]
+for i in range(len(file_read_proc)):
+    filedata = File_data ( file_list_r[i]
+                       ,text_read[i]
+                       ,text_read_proc[i]
+                       ,file_list_size[i]
+                       ,file_list_last[i]
                        )
     list_faildata.append(filedata)
 
@@ -173,26 +181,53 @@ datafile_list = File_data_list(list_faildata)
 
 # проверка преобразования класса в словарь
 dictlist = filedatalist_to_dict(datafile_list)
+print(f'Результат перевода объекта datafile_list в словарь -----')
 pprint(dictlist)
 
 #Смена текщей дрректории . на # Смена текщей дрректории . на output/
-os.chdir('..')
-print (f'смена текущей директории на: {os.getcwd()}')
-os.chdir('output/')
+#Смена текущей директории
+os.chdir( maindir)
+os.chdir('poject_root')
+os.chdir('output')
 print (f'смена текущей директории на: {os.getcwd()}')
 
 filew = 'processed_data.json'
-print(filew)
-
 with open(filew, 'w', encoding = 'utf-8') as file:
     json.dump(dictlist, file)
-
 with open(filew, 'r', encoding = 'utf-8') as file:
     loaded_data = json.load(file )
 
-print (type(loaded_data))
-
+print (print(f'Результат десериализации joson -----'))
 pprint(loaded_data)
 
+#Смена текущей директории
+os.chdir( maindir)
+os.chdir('poject_root')
+filelog = 'logs/dz_log.log'
+print (f'смена текущей директории на: {os.getcwd()}\n')
+
+for i in range(len(message_log1)):
+    message = message_log1[i]
+    write_log(filelog, message)
+
+for i in range(len(message_log2)):
+    message = message_log2[i]
+    write_log(filelog, message)
+
+message = (f'Списки  текстов составлены\n')
+write_log(filelog, message)
+
+message = f'Список размеров файлов в байтах составлен\n'
+write_log(filelog, message)
+
+message = f'Список дат последнего изменеия файлов заполнен\n'
+write_log(filelog, message)
+
+message = f'Oбъект - Cписок объектов данные файла сохдан\n'
+write_log(filelog, message)
 
 
+message = f'проведена сериализация в файл {filew}\n'
+write_log(filelog, message)
+message = f'проведена десериализация из файла {filew}\n'
+write_log(filelog, message)
